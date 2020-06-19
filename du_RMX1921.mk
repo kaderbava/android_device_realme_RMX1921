add76eb1540454bc7a9aa6f26b1f54f360bd6b64
kaderbava <ksbava7325@gmail.com>
2020-06-19 19:29:40 +0000

    RMX1921: Corvus-ify

--- du_RMX1921.mk
+++ du_RMX1921.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/du/config/common_full_phone.mk)
 # Inherit from RMX1921 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# FOD Animation
+EXTRA_FOD_ANIMATIONS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1921
 PRODUCT_NAME := du_RMX1921
